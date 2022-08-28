da06788a912d9e98f54986431df84091c3be593e
mathildeew <mathildewiik@hotmail.com>
2022-08-28 16:15:49 +0200

    s

--- js/productPage.js
+++ js/productPage.js
@@ -5,28 +5,26 @@ const id = params.get("id");
 console.log(id);
 
 const url = "https://flower-power.site/wp-json/wc/store/products/" + id;
-const jacketSection = document.querySelector(".jacket-section")
-const headingRight = document.querySelector(".heading-right")
+const jacketSection = document.querySelector(".jacket-section");
+const headingRight = document.querySelector(".heading-right");
 
 async function getJacket(url) {
-    const response = await fetch(url);
-    const jacket = await response.json();
-    console.log(jacket);
+  const response = await fetch(url);
+  const jacket = await response.json();
+  console.log(jacket);
 
-    jacketSection.innerHTML += ` 
+  jacketSection.innerHTML += ` 
     <div class="heading-center">
         <h1>${jacket.name}</h1>
         <h2>${jacket.prices.currency_code} ${jacket.prices.price}</h2>
     </div>
-    <img src="${jacket.images[1].src}">
-  `
+    <img src="${jacket.images[0].src}">
+  `;
 
   headingRight.innerHTML += `
   <h1>${jacket.name}</h1>
   <h2>${jacket.prices.currency_code} ${jacket.prices.price}</h2>
-  `  
+  `;
 }
 
 getJacket(url);
-
- 
